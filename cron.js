20bebd62cebb60adb93732fc3bef853f8054bf45
tumGER <25822956+tumGER@users.noreply.github.com>
2018-08-10 13:42:30 +0200

    Update to have easier way to store payload

--- cron.js
+++ cron.js
@@ -84,7 +84,7 @@ function writeLatestStableVersion(err, result) {
 
 function bundleLatestStableVersion() {
     archive(__dirname + '/SDFilesSwitch/Hekate', __dirname + '/res/hekate-stable.zip')
-    archive(__dirname + '/SDFilesSwitch/ReiNX', __dirname + '/res/reinx-stable.zip')
+    archive(__dirname + '/SDFilesSwitch/ReiNX/SD', __dirname + '/res/reinx-stable.zip')
 }
 
 // Bleeding Edge
@@ -107,7 +107,7 @@ function writeLatestBleedingEdgeVersion(err, result) {
 
 function bundleLatestBleedingEdgeVersion() {
     archive(__dirname + '/SDFilesSwitch/Hekate', __dirname + '/res/hekate-bleedingedge.zip')
-    archive(__dirname + '/SDFilesSwitch/ReiNX', __dirname + '/res/reinx-bleedingedge.zip')
+    archive(__dirname + '/SDFilesSwitch/ReiNX/SD', __dirname + '/res/reinx-bleedingedge.zip')
 }
 
 // Helper Functions
