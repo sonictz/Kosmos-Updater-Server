45d9e5807281ce3606988515bce9061b9a796913
Steven Mattera <me@stevenmattera.com>
2018-08-10 08:18:59 -0400

    Small minor changes.

--- v2Api.js
+++ v2Api.js
@@ -1,3 +1,20 @@
+// SDFile Updater Server
+// Copyright (C) 2018 Steven Mattera
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
@@ -68,9 +85,7 @@ router.get('/download/:launcher/:channel', (req, res) => {
 })
 
 function validateLauncher(launcher) {
-    if (launcher.toLowerCase() === 'hekate') {
-        return 'hekate'
-    } else if (launcher.toLowerCase() === 'layeredfs') {
+    if (launcher.toLowerCase() === 'hekate' || launcher.toLowerCase() === 'layeredfs') {
         return 'hekate'
     } else if (launcher.toLowerCase() === 'reinx') {
         return 'reinx'
